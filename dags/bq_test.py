4b7d9511f1f0ed1e04e0f6e38022206a0c250c15
Thomas Wrigley <45482876+thomaswrigleyza@users.noreply.github.com>
2024-12-01 14:16:12 +0200

    Testing BigQuery connection

---
+++ dags/bq_test.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='test_bigquery_connection',
+    default_args=default_args,
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None, 
+    catchup=False,
+) as dag:
+
+    test_bq_query = BigQueryInsertJobOperator(
+        task_id='run_test_query',
+        configuration={
+            "query": {
+                "query": "SELECT * FROM `utility-replica-441110-u8.openweather_data.src_openweather_aqi` LIMIT 10",
+                "useLegacySql": False,
+            }
+        },
+        location="US",
+        gcp_conn_id='bq_airflow',
+    )
