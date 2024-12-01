bb6ff3f106646c46c51207a713136a9af4811e5b
Thomas Wrigley <45482876+thomaswrigleyza@users.noreply.github.com>
2024-12-01 17:47:23 +0200

    GCP connection debugging

--- dags/bq_test.py
+++ dags/bq_test.py
@@ -1,5 +1,7 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.operators.python_operator import PythonOperator
+from google.cloud import bigquery
+from google.oauth2 import service_account
 from datetime import datetime
 
 default_args = {
@@ -10,21 +12,30 @@ default_args = {
     'retries': 1,
 }
 
+def test_bigquery():
+    # Authenticate with the keyfile
+    keyfile_path = "/Users/thomaswrigley/Code/Personal/InsightEngine/Keys/airflow_bq_key.json"
+    credentials = service_account.Credentials.from_service_account_file(keyfile_path)
+
+    # Initialize BigQuery client
+    client = bigquery.Client(credentials=credentials, project="utility-replica-441110-u8")
+
+    # Run a query
+    query = "SELECT * FROM `utility-replica-441110-u8.openweather_data.src_openweather_aqi` LIMIT 10"
+    results = client.query(query).result()
+
+    for row in results:
+        print(row)
+
 with DAG(
-    dag_id='test_bigquery_connection',
+    dag_id='test_bigquery_python_auth',
     default_args=default_args,
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None, 
+    schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    test_bq_query = BigQueryInsertJobOperator(
-        task_id='run_test_query',
-        configuration={
-            "query": {
-                "query": "SELECT * FROM `utility-replica-441110-u8.openweather_data.src_openweather_aqi` LIMIT 10",
-                "useLegacySql": False,
-            }
-        },
-        location="US",
+    python_task = PythonOperator(
+        task_id='run_python_query',
+        python_callable=test_bigquery,
     )
